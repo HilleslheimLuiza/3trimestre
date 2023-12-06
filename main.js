5908fdb8432c4e45bd097bdfa490bcff6cbf2516
Hilleslheim <108075498+HilleslheimLuiza@users.noreply.github.com>
2023-12-06 14:48:29 -0300

    0612

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
-for ( let contador  = 0; contador < listaDeTeclas.length; contador++)  {
+for ( let contador  = 0; contador < listaDeTeclas.length; contador++) {
      
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1]; 
@@ -16,7 +16,18 @@ for ( let contador  = 0; contador < listaDeTeclas.length; contador++)  {
         tocaSom(idAudio);
     }
    
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
-    } 
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == 'Space')
+     
+        if (evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
